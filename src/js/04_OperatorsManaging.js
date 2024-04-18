131b04d628f3fbeee9fa441579bc6e3b9f83ade8
MajoPino <majo12pino@gmail.com>
2024-04-18 07:09:30 -0500

    Basic data types on js

--- src/js/04_OperatorsManaging.js
+++ src/js/04_OperatorsManaging.js
@@ -1,12 +1,17 @@
 
-//Data type on Javascript
-
+//Basic data types on Javascript
 let variable1 = 12
 let variable2 = "4"
 let variable3 = "Hello world"
 let variable4 = true
 let variable5 = null
 let variable6 = undefined
+let variable7 = NaN //Property of number, not a data type
+let variable8 = [1, 2, 3, 4, 5, 6]
+let variable9 = {"name": "majo", "lastName": "pino", "age": 19}
+let variable10 = function () {
+    console.log("Hello world")
+}
 
 console.log("variable 1", typeof variable1)
 
@@ -19,3 +24,13 @@ console.log("variable 4", typeof variable4)
 console.log("variable 5", typeof variable5)
 
 console.log("variable 6", typeof variable6)
+
+console.log("variable 7", typeof variable7) //Property of number, not a data type
+
+console.log("variable 8", typeof variable8)
+
+console.log("variable 9", typeof variable9)
+
+console.log("variable 10", typeof variable10)
+
+/* ----------------------------------------------------------------------------------------- */
