1b9347da25f2f69a66c8c732c0bad3cb960e6053
MajoPino <majo12pino@gmail.com>
2024-04-18 07:01:26 -0500

    Creating of the js file of april 18 class

---
+++ src/js/04_OperatorsManaging.js
@@ -0,0 +1,21 @@
+
+//Data type on Javascript
+
+let variable1 = 12
+let variable2 = "4"
+let variable3 = "Hello world"
+let variable4 = true
+let variable5 = null
+let variable6 = undefined
+
+console.log("variable 1", typeof variable1)
+
+console.log("variable 2", typeof variable2)
+
+console.log("variable 3", typeof variable3)
+
+console.log("variable 4", typeof variable4)
+
+console.log("variable 5", typeof variable5)
+
+console.log("variable 6", typeof variable6)
